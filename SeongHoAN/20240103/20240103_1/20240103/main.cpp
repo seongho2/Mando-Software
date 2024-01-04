41820b7ad84aedc7885a024920907266c110c249
seongho2 <125865161+seongho2@users.noreply.github.com>
2024-01-04 13:19:15 +0900

    Add files via upload

---
+++ SeongHoAN/20240103/20240103_1/20240103/main.cpp
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+/*
+int get_interger(void)
+{
+	int n;
+
+	printf("정수를 입력하시오: ");
+	scanf("%d", &n);
+	return n;
+}
+int is_prime(int n)
+{
+	int i;
+	
+	for (i = 2; i < n; i++)
+	{
+		if (n% i == 0)
+			return 0;
+	}
+	return 1;
+}
+int main(void)
+{
+	int n;
+	n = get_interger();
+
+	if (is_prime(n) == 1)
+		printf("% d는 소수입니다.\n", n);
+	else
+		printf("%d는 소수가 아닙니다.\n", n);
+	return 0;
+}
+
+double c_to_f(double c_temp);
+
+int main(void)
+{
+	printf("섭씨 %.2lf도는 화씨 %.2lf입니다.\n", 36.0, c_to_f(36.0));
+}
+
+double c_to_f(double c_temp)
+{
+	return 9.0 / 5.0 * c_temp + 32;
+}
+
+long factorial(int n)
+{
+	printf("factorial(%d)\n", n);
+
+	if (n <= 1)return 1;
+	else return n * factorial(n - 1);
+}
+
+int main(void)
+{
+	int n = 0;
+	long f;
+
+	printf("정수를 입력하시오: ");
+	scanf("%d", &n);
+	printf("%d d!은 %d입니다.\n", n, factorial(n));
+	return 0;
+}*/
